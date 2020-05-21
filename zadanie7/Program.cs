9b737b30a6dba3a4472f3df8f58052b96b2f7383
leenarie <KATIDARK@MAIL.RU>
2020-05-21 23:24:54 +0500

    Добавьте файлы проекта.

---
+++ zadanie7/Program.cs
@@ -0,0 +1,50 @@
+﻿
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace zadanie5
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Student Andrey = new Student();
+                Andrey.AddStudent(1, "Иванов Андрей Владимирович", "19-11-2", "25.12.2001");
+                Andrey.ChangeGroup("19-11-1");
+                Andrey.StudentWrite();
+                Student Eva = new Student();
+                Eva.AddStudent(2, "Коновалова Ева Ивановна", "18-11-3", "16.06.2000");
+                Student Anna = new Student();
+                Anna.AddStudent(3, "Огурцова Анна Николаевна", "18-11-1", "07.01.2000");
+                Student Oleg = new Student();
+                Oleg.AddStudent(3, "Иванов Олег Сергеевич", "21-11-1", "07.12.2003");
+                Student Sasha = new Student();
+                Sasha.AddStudent(3, "Иранов Александр Сергеевич", "21-11-1", "08.10.2003");
+            //задание6
+            Student.WriteAll();
+                Student.WriteAllId(2);
+                Student.WriteAge(3);
+                //задание 7
+                Student.StudentWriteFioShort(Andrey);
+            List<Student> less18 = Student.Around18('s');
+            Student.WriteAll(less18);
+            Console.WriteLine("______");
+            List<Student> more18 = Student.Around18('a');
+                Student.WriteAll(more18);
+            Console.WriteLine("______");
+            Student.WriteAll(Student.FamSearch("Иванов"));
+            Console.WriteLine("______");
+            Student.WriteAll(Student.FamStartSearch("И"));
+            Console.WriteLine("______");
+            Student.WriteAll(Student.FamStartSearch("Ив"));
+            Console.ReadKey();
+
+
+            }
+        }
+    }
+
---
+++ zadanie7/Student.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace zadanie5
+{
+    class Student
+    {
+        static int id;
+        static string Fio;
+        static string group;
+        static string data;
+
+         public ArrayList studinfo = new ArrayList(4);
+         static public List<Student> students = new List<Student>();
+        public  int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+               if (value>=0) id = value;
+            }
+        }
+
+       public string FIO
+        {
+            get
+            {
+                return Fio;
+            }
+
+            set
+            {
+                if (!value.Trim().Equals("")) Fio = value;
+            }
+        }
+
+        public string Group
+        {
+            get
+            {
+                return group;
+            }
+
+            set
+            {
+                if (!value.Trim().Equals("")) group = value;
+            }
+        }
+
+        public string Data
+        {
+            get
+            {
+                return data;
+            }
+
+            set
+            {
+                data = value;
+            }
+        }
+
+        public void AddStudent(int id,string Fio, string group,string data)
+        {
+
+            studinfo.Add(id);
+            studinfo.Add(Fio);
+            studinfo.Add(group);
+            studinfo.Add(data);
+            students.Add(this);
+        }
+
+        public void AddStudent(Student s)
+        {
+            students.Add(s);
+        }
+
+        public void StudentWrite()
+        {
+            foreach (object o in studinfo)
+            {
+                Console.WriteLine(o + " ");
+            }
+        }
+        static public void StudentWriteFio(Student s)
+        {
+
+            Console.WriteLine(s.studinfo[1]);
+            
+        }
+
+        static public void StudentWriteFioShort(Student s)
+        {
+            string[] str = s.studinfo[1].ToString().Split(' ');
+
+            Console.WriteLine(str[0] + " " + str[1][0] + ". " + str[2][0] + ".");
+
+        }
+
+        static public int StudentAge(Student s)
+        {
+
+            DateTime Date = DateTime.ParseExact(s.studinfo[3].ToString(), "dd.mm.yyyy", null);
+            DateTime now = DateTime.Now;
+            TimeSpan age = now.Subtract(Date);
+            int years = (int)age.TotalDays;
+            years = years / 365;
+            return years;
+
+        }
+        public void ChangeId(int id)
+        {
+            studinfo[0] = id;
+        }
+
+         public void ChangeFio(string Fio)
+        {
+            studinfo[1] = Fio;
+        }
+         public void ChangeGroup(string group)
+        {
+            studinfo[2] = group;
+        }
+         public void ChangeData(string data)
+        {
+            studinfo[3] = data;
+        }
+
+         public void RemoveStudent()
+        {
+            students.Remove(this);
+            studinfo.Clear();
+
+        }
+        static public void WriteAll()
+        {
+            
+            foreach ( Student o in students)
+            {
+                StudentWriteFio(o);
+            }
+        }
+
+        static public void WriteAll(List<Student> st)
+        {
+
+            foreach (Student o in st)
+            {
+                StudentWriteFio(o);
+            }
+        }
+
+        static public void WriteAllId(int id_)
+        {
+            foreach (Student o in students)
+            {
+                if ( id_ == (int)o.studinfo[0])
+                {
+                    o.StudentWrite();
+                }
+
+            }
+        }
+
+       static public void WriteAge(int id_)
+        {
+            foreach (Student o in students)
+            {
+                if (id_ == (int)o.studinfo[0])
+                {
+                    Console.WriteLine(StudentAge(o));
+                }
+
+            }
+        }
+
+        static public List<Student> Around18(char c)
+        {
+            List<Student> stmas = new List<Student>();
+            if (c.Equals('a'))
+            {
+                foreach (Student o in students)
+                {
+                    if(StudentAge(o) >= 18)
+                    {
+                        stmas.Add(o);
+                    }
+                }
+
+            }
+            else if (c.Equals('s'))
+            {
+                foreach (Student o in students)
+                {
+                    if (StudentAge(o) < 18)
+                    {
+                        stmas.Add(o);
+                    }
+                }
+            }
+            return stmas;
+        }
+
+        static public List<Student> FamSearch(string fam)
+        {
+            List<Student> stmas = new List<Student>();
+            
+
+                foreach (Student o in students)
+                {
+                string[] str = o.studinfo[1].ToString().Split(' ');
+                if (str[0].Trim().Equals(fam))
+                    {
+                        stmas.Add(o);
+                    }
+                }
+
+            return stmas;
+        }
+
+        static public List<Student> FamStartSearch(string fam)
+        {
+            List<Student> stmas = new List<Student>();
+
+            foreach (Student o in students)
+            {
+                string str = o.studinfo[1].ToString();
+                int c = 0;
+                for (int i = 0; i < fam.Length; i++)
+                {
+                    if (str[i].Equals(fam[i]))
+                    {
+                        c++;
+                    }
+                }
+                if (c == fam.Length)
+                {
+                    stmas.Add(o);
+                }
+            }
+
+            return stmas;
+        }
+
+    }
+}
